c7844fbf5e83c86384e26d5903b3b32b4aa341e4
jayendrant2007 <jayendrant2007@gmail.com>
2025-12-19 01:31:26 +0000

    Add Smart City work management app (server + client)

---
+++ server/company.js
@@ -0,0 +1,8 @@
+export const COMPANY = {
+  Name: 'SMART CITY TECHNOLOGIES PTE LTD',
+  Address: '24 sin ming lane, 05-97, Singapore 573970',
+  Phone: '63340543',
+  Email: 'services@smartcitytechnologies.com.sg',
+  Web: 'smartcitytechnologies.com.sg',
+  Whatsapp: '80458281'
+};
